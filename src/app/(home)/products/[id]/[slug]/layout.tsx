e77fc3b5da3c652498dc098ff23f6741c32a9532
Josh06 <Admin@chaucongtu.gq>
2023-12-15 14:16:36 +0700

    update

--- src/app/(home)/products/[id]/[slug]/layout.tsx
+++ src/app/(home)/products/[id]/[slug]/layout.tsx
@@ -1,36 +1,41 @@
-import { Metadata, ResolvingMetadata } from "next"
+import { Metadata, ResolvingMetadata } from "next";
 
 type Params = {
-    id: string;
+  id: string;
 };
 
 type SearchParams = {
-    [key: string]: string | string[] | undefined;
+  [key: string]: string | string[] | undefined;
+};
+
+type Props = {
+  params: Params;
+  searchParams: SearchParams;
 };
 
 export async function generateMetadata(
-    { params, searchParams }: { params: Params; searchParams: SearchParams },
-    parent: ResolvingMetadata
+  { params, searchParams }: Props,
+  parent: ResolvingMetadata
 ): Promise<Metadata> {
-    const id = params.id;
+  const id = params.id;
 
-    const product = await fetch(`http://127.0.0.1:8000/api/products/${id}`).then((res) => res.json());
+  const product = await fetch(`http://127.0.0.1:8000/api/products/${id}`).then((res) => res.json());
 
-    const previousImages = (await parent).openGraph?.images || [];
+  const previousImages = (await parent).openGraph?.images || [];
 
-    return {
-        title: product.name + '| MixiShop',
-        description: "Mua ngay sản phẩm " + product.name + " với siêu ưu đãi ngay bây giờ.",
-        openGraph: {
-            images: ['/some-specific-page-image.jpg', ...previousImages],
-        },
-    };
+  return {
+    title: product.name + '| MixiShop',
+    description: "Mua ngay sản phẩm " + product.name + " với siêu ưu đãi ngay bây giờ.",
+    openGraph: {
+      images: ['/some-specific-page-image.jpg', ...previousImages],
+    },
+  };
 }
 
 export default function RootLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-    return <>{children}</>;
+  return <>{children}</>;
 }
